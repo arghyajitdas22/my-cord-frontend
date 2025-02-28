c12ae5c1bd83b9005bf5ce0f797a13fed0d6506f
Arghyajit Das <116450956+arghyajitdas22@users.noreply.github.com>
2025-02-28 21:46:51 +0530

    added react-router v7

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <main className="bg-red-500">Hello Mycord</main>
+      <main className="bg-[url(/src/assets/auth-bg.svg)] bg-cover bg-no-repeat max-w-screen min-h-screen">
+        Hello Mycord
+      </main>
     </>
   );
 }
