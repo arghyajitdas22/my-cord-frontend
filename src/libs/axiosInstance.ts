ff263082bf4ebf6f8f0aaa57d4551ba3c0afc0e4
Arghyajit Das <116450956+arghyajitdas22@users.noreply.github.com>
2025-03-10 03:46:26 +0530

    added libs folder for third-party librar config

---
+++ src/libs/axiosInstance.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  async (error) => {
+    console.log(error);
+  }
+);
+
+export default axiosInstance;
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,8 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import AuthRegister from "./pages/auth/AuthRegister.tsx";
 import AuthLogin from "./pages/auth/AuthLogin.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./libs/queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
