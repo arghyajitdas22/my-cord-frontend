90cc68cf0355b5911aac5eb79b16f9dd2005edaf
Arghyajit Das <116450956+arghyajitdas22@users.noreply.github.com>
2025-05-29 17:52:13 +0530

    added user control box

---
+++ src/components/common/UserControl.tsx
@@ -0,0 +1,54 @@
+import {
+  DiscordLogo,
+  Microphone,
+  SignOut,
+  VideoCamera,
+} from "@phosphor-icons/react";
+import * as React from "react";
+import { useUser } from "../../hooks/useUser";
+
+interface IUserControlProps {}
+
+const UserControl: React.FunctionComponent<IUserControlProps> = () => {
+  const user = useUser((state) => state.user);
+  return (
+    <div className="w-[380px] px-3 py-2 rounded-md bg-[#2f3136] flex items-center justify-between absolute left-5 bottom-5">
+      {/* icon display name and username */}
+      <div className="flex items-center gap-x-4">
+        <div className="w-10 h-10 rounded-full bg-[#ed5555] flex items-center justify-center">
+          <DiscordLogo size={20} weight="fill" color="white" />
+        </div>
+        <div className="flex flex-col gap-y-1">
+          <span className="text-sm text-white">{user?.displayName}</span>
+          <span className="text-xs text-gray-300">{user?.username}</span>
+        </div>
+      </div>
+      {/* user control of video, mic and logout */}
+      <div className="flex items-center gap-x-2">
+        {/* camera */}
+        <button
+          type="button"
+          className="flex items-center justify-center p-2 rounded-sm hover:bg-[#4a4c51] transition-all duration-150 ease-in-out cursor-pointer"
+        >
+          <VideoCamera size={20} fill="bold" />
+        </button>
+        {/* mic */}
+        <button
+          type="button"
+          className="flex items-center justify-center p-2 rounded-sm hover:bg-[#4a4c51] transition-all duration-150 ease-in-out cursor-pointer"
+        >
+          <Microphone size={20} fill="bold" />
+        </button>
+        {/* logout */}
+        <button
+          type="button"
+          className="flex items-center justify-center p-2 rounded-sm hover:bg-[#4a4c51] transition-all duration-150 ease-in-out cursor-pointer"
+        >
+          <SignOut size={20} fill="bold" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UserControl;
--- src/layouts/common/common.layout.tsx
+++ src/layouts/common/common.layout.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import SideNav from "../../components/common/SideNav";
+import UserControl from "../../components/common/UserControl";
 
 interface ICommonLayoutProps {}
 
 const CommonLayout: React.FunctionComponent<ICommonLayoutProps> = () => {
   return (
-    <article className="bg-[#202225] h-screen overflow-hidden w-screen">
+    <article className="bg-[#202225] h-screen overflow-hidden w-screen relative">
       <header className="w-full py-2 text-center text-white text-base uppercase">
         my cord
       </header>
+
+      <UserControl />
 
       <div className="flex">
         <SideNav />
